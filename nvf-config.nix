207742e7266cfabd7ba76a4e6658be07fe704f5d
Savio Mendes <savio.c.mendes@gmail.com>
2026-01-12 16:56:56 -0300

    feat: change transparency

--- nvf-config.nix
+++ nvf-config.nix
@@ -2,26 +2,29 @@
 {
     config.vim = let p = pkgs.vimPlugins; in {
     # Enable custom theming options
-        theme.enable = true;
-        theme.base16-colors = {
-            base00= "#1c1213";
-            base01= "#3a2425";
-            base02= "#563837";
-            base03= "#6d4745";
-            base04= "#8b5d57";
-            base05= "#a87569";
-            base06= "#c58d7b";
-            base07= "#e3a68c";
-            base08= "#c24f57";
-            base09= "#a63650";
-            base0A= "#f28171";
-            base0B= "#806c61";
-            base0C= "#6b6566";
-            base0D= "#684c59";
-            base0E= "#a63650";
-            base0F= "#893f45";
+        theme = {
+            enable = true;
+            base16-colors = {
+                base00= "#1c1213";
+                base01= "#3a2425";
+                base02= "#563837";
+                base03= "#6d4745";
+                base04= "#8b5d57";
+                base05= "#a87569";
+                base06= "#c58d7b";
+                base07= "#e3a68c";
+                base08= "#c24f57";
+                base09= "#a63650";
+                base0A= "#f28171";
+                base0B= "#806c61";
+                base0C= "#6b6566";
+                base0D= "#684c59";
+                base0E= "#a63650";
+                base0F= "#893f45";
+            };
+            name = "base16";
+            transparent = true;
         };
-        theme.name = "base16";
 
         utility.icon-picker.enable = true;
 
